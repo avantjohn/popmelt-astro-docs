9aa2cb803b561630f6180e0f24e1665edf0f3cf9
John Jones <john@popmelt.com>
2024-12-01 20:19:23 -0600

    new project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        react(),
-        tailwind({
-            applyBaseStyles: false
-        })
-    ],
+    devToolbar: {
+        enabled: false
+    },
+    integrations: [react(), icon()],
     output: 'hybrid',
     adapter: netlify()
 });
